f684dae758b80e8193be6d7fbc374c79e1b3da13
unknown <jeshuarcr@gmail.com>
2019-01-22 01:04:01 -0400

    db url

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const heroku_url =  process.env.DATABASE_URL || 'postgres://kjrfttfynoghsc:74a95068d0d00a672f95e4c084abaac484b39fd8821aa94b13f22682b1fbc7e6@ec2-54-225-98-131.compute-1.amazonaws.com:5432/d9avpgfr1tidjr?ssl=true'
+const heroku_url =  process.env.DATABASE_URL || 'postgres://arorrxllnqaega:945f31cd5883f6386e099c3891343e5993d425beae84acb2227fe4906fa3ed3a@ec2-54-235-68-3.compute-1.amazonaws.com:5432/dbi340b5kb69eu'
 module.exports = {
   basePath: '/',
   port: process.env.PORT || 3000,
